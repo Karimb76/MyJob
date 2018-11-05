19fb741c6c9f92f88357b6888833502f87dc340b
Karl <badik76@gmail.Comgit>
2018-11-05 12:22:07 +0100

    modif

--- app/route.js
+++ app/route.js
@@ -4,5 +4,13 @@ app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "view/produits.html"
+    })
+    // navbar master
+    .when("/produits", {
+        templateUrl : "view/produits.html"
+    })
+    // navbar left
+    .when("/produits/pier-huge/", {
+        templateUrl : "view/produits/pier-huge.html"
     });
 });
