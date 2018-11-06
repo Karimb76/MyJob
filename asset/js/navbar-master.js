c28d16b3398660c55b68f7f5828d64b4db14940e
Robinho <robin.halberda@orange.fr>
2018-11-06 15:36:58 +0100

    fusion

---
+++ asset/js/navbar-master.js
@@ -0,0 +1,17 @@
+// début de la navbar
+//Menu toggle-button for small screens
+$(document).ready(function (){
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+//Scrolling Effect for nav
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+});
+// fin de la navbar
