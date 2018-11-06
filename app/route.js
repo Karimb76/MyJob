c70e344331b9b61b484c4e1ce091a0418590ddce
Karl <badik76@gmail.Comgit>
2018-11-06 13:48:44 +0100

    article v2

--- app/route.js
+++ app/route.js
@@ -3,12 +3,16 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "view/produits.html"
+        templateUrl : "view/produits.html",
         controller : "prodCtrl"
     })
     // navbar master
-    // .when("/service", {
-    //     templateUrl : "view/service.html",
-    //     controller : "prodCtrl"
-    // })
+    .when("/service", {
+        templateUrl : "view/service.html",
+        controller : "prodCtrl"
+    })
+    // navbar left
+    .when("/produits", {
+        templateUrl : "view/produits.html"
+    });
 });
