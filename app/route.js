97cfeab1aa59232865dfb804591adad7b1276ef5
codeurh24 <cci.corlouer@gmail.com>
2018-11-06 13:49:47 +0100

    modif de codeur pour que la view fonctionne

--- app/route.js
+++ app/route.js
@@ -3,7 +3,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "view/produits.html"
+        templateUrl : "view/produits.html",
         controller : "prodCtrl"
     })
     // navbar master
