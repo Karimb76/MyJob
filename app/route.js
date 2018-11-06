79e3671f29bb8e0c492e92f13b45b82a44b9ca69
codeurh24 <cci.corlouer@gmail.com>
2018-11-06 13:42:21 +0100

    commit avant de changer de branch kralbrancheV2

--- app/route.js
+++ app/route.js
@@ -4,18 +4,5 @@ app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "view/produits.html"
-    })
-    // navbar master
-    .when("/service", {
-        templateUrl : "view/service.html",
-        controller : "prodCtrl"
-    })
-    .when("/produits", {
-        templateUrl : "view/produits.html",
-        controller : "prodCtrl"
-    })
-    // navbar left
-    .when("/produits/pier-huge/", {
-        templateUrl : "view/produits/pier-huge.html"
     });
 });
